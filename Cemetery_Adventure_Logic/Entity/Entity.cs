0bd5f8bfac34ce87a8b773734976cd206ccf2852
Ostin-Oskarose <oskarcraft@gmail.com>
2023-03-21 14:43:13 +0100

    Started implementing Entity class

---
+++ Cemetery_Adventure_Logic/Entity/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Cemetery_Adventure_Logic.Entity;
+
+public abstract class Entity
+{
+    protected int X { get; private set; }
+    protected int Y { get; private set; }
+    protected int HP { get; private set; }
+    protected int MaxHP { get; private set; }
+    protected int Damage { get; private set; }
+    protected int Defense { get; private set; }
+
+    public virtual void Move(int x, int y)
+    {
+        X = x; 
+        Y = y;
+    }
+}
