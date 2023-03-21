d8ef5b835a72cc2e6caf85ca7c6164357dfad37b
Ostin-Oskarose <oskarcraft@gmail.com>
2023-03-21 14:51:34 +0100

    Started implementing Player class

---
+++ Cemetery_Adventure_Logic/Entity/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace Cemetery_Adventure_Logic.Entity;
+
+public class Player : Entity
+{
+    public (int X, int Y) GetMove()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Turn()
+    {
+        var move = GetMove();
+        Move(move.X, move.Y);
+    }
+}
