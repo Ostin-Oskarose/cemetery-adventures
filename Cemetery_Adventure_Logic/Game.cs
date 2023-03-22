f1dc794ab6be372dfbf3b8a5c28663a4c81252a9
Ostin-Oskarose <oskarcraft@gmail.com>
2023-03-22 13:41:26 +0100

    Added floor field to Game

--- Cemetery_Adventure_Logic/Game.cs
+++ Cemetery_Adventure_Logic/Game.cs
@@ -8,12 +8,14 @@ namespace Cemetery_Adventure_Logic
         private int _width = 50;
         private int _height = 40;
         private Player _player;
+        private int Floor;
 
         public Board GameBoard { get; set; }
 
         public Game()
         {
             _player = new Player("Player", (1, 1), 10, 1, 0);
+            Floor = 1;
             GameBoard = new Board(_height, _width, _player);
         }
 
