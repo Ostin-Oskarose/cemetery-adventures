acceb24205fa71c431488505873f7eef224713d1
Ostin-Oskarose <oskarcraft@gmail.com>
2023-04-06 14:49:44 +0200

    Added tests for character

---
+++ UnitTesting/CharacterTests.cs
@@ -0,0 +1,27 @@
+﻿using Cemetery_Adventure_Logic.Entity.Character;
+using Moq;
+
+namespace UnitTests;
+
+public class CharacterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void IncreaseMaxHp_PositiveValue_IncreasesCharactersMaxHp()
+    {
+        var character = new Mock<Character>(MockBehavior.Strict, new object[] { "Name", (1, 1), 20, 5, 1 });
+        character.Object.IncreaseMaxHP(5);
+        Assert.That(character.Object.MaxHP, Is.EqualTo(25));
+    }
+
+    [Test]
+    public void IncreaseMaxHp_NegativeValue_ThrowsException()
+    {
+        var character = new Mock<Character>(MockBehavior.Strict, new object[] { "Name", (1, 1), 20, 5, 1 });
+        Assert.Throws<ArgumentException>(() => character.Object.IncreaseMaxHP(-5));
+    }
+}
