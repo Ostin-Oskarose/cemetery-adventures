0d96e1bcab97a03a51c843c026e194ed4934a394
Ostin-Oskarose <oskarcraft@gmail.com>
2023-04-07 09:22:09 +0200

    Added test for character move

--- UnitTesting/CharacterTests.cs
+++ UnitTesting/CharacterTests.cs
@@ -24,4 +24,18 @@ public class CharacterTests
         var character = new Mock<Character>(MockBehavior.Strict, new object[] { "Name", (1, 1), 20, 5, 1 });
         Assert.Throws<ArgumentException>(() => character.Object.IncreaseMaxHP(-5));
     }
+
+    [Test]
+    [TestCase(1,1)]
+    [TestCase(-1,-1)]
+    public void Move_ValidValues_ChangesCoordinates(int x, int y)
+    {
+        var character = new Mock<Character>(MockBehavior.Default, new object[] { "Name", (1, 1), 20, 5, 1 });
+        character.Object.Move(x, y);
+        Assert.Multiple(() =>
+        {
+            Assert.That(character.Object.Position.X, Is.EqualTo(x));
+            Assert.That(character.Object.Position.Y, Is.EqualTo(y));
+        });
+    }
 }
